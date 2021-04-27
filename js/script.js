3dde95b9060210ea87a46e33c0a6efc1bf11f38f
Glitch (lily-stitch-witch) <none>
2021-04-27 22:57:16 +0000

    🍓🎰 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -2,9 +2,19 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
+var giphyLink = `https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC`;
 
 $(".search-button").click(function(){
-  
+  fetch(giphyLink)
+
+        .then(function(response) {
+        return response.json();
+        })
+
+        .then(function(data) {
+        console.log(data.data[0].images.original.url);
+        $('').append();
+        });
 
   
 });

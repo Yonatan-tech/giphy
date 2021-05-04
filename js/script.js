b6bd1bd06ea3a79f456e7327131a21ded4125b99
Glitch (lily-stitch-witch) <none>
2021-05-04 22:58:31 +0000

    display random image of key word

--- js/script.js
+++ js/script.js
@@ -2,20 +2,30 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
-var giphyLink = `https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC`;
+
 
 $(".search-button").click(function(){
+  let userInput = $('.search-term').val();
+  var giphyLink = `https://api.giphy.com/v1/gifs/search?q=${userInput}&rating=pg&api_key=dc6zaTOxFJmzC`;
+  let randomNumbers = Math.random();
+  
   fetch(giphyLink)
+  
+  
 
         .then(function(response) {
         return response.json();
         })
 
         .then(function(data) {
-        console.log(data.data[0].images.original.url);
+        console.log(data.data[Math.floor(Math.random() * data.data.length)].images.original.url);
+        
         $('').append();
+        $(".main").append("<img src =" + (data.data[Math.floor(Math.random() * data.data.length)].images.original.url) + ">");
         });
 
+ 
   
 });
 
+//Math.floor(Math.random() * 10);
